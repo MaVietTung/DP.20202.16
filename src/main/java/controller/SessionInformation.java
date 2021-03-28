2d87228eb5573fdba1d0d561deb3b97ff784911c
MaVietTung <MaVietTung>
2021-03-28 21:11:21 +0700

    Using SIngleton Design Pattern

--- src/main/java/controller/SessionInformation.java
+++ src/main/java/controller/SessionInformation.java
@@ -8,10 +8,20 @@ import java.time.LocalDateTime;
 /**
  * @author
  */
+//Singleton: Co the ap dung mau thiet ke Singleton vao lop SessionInfomation de dam bao trong mot phien chi co mot the hien cua cac thong tin mot phien
 public class SessionInformation {
 
-    public static User mainUser;
-    public static Cart cartInstance = new Cart();
-    public static LocalDateTime expiredTime;
+    public   User mainUser;
+    public   Cart cartInstance = new Cart();
+    public   LocalDateTime expiredTime;
 
+    private static SessionInformation singleton;
+    private SessionInformation(){}
+
+    public static SessionInformation getInstance(){
+        if (SessionInformation.singleton == null){
+            SessionInformation.singleton = new SessionInformation();
+        }
+        return SessionInformation.singleton;
+    }
 }
