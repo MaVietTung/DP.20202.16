ccf595616558de1a72acadc997387222f1796ba0
vietnb <vietlan2511@gmail.com>
2021-05-20 16:14:33 +0700

    update strategy pattern

--- src/main/java/views/screen/payment/PaymentScreenHandler.java
+++ src/main/java/views/screen/payment/PaymentScreenHandler.java
@@ -5,8 +5,11 @@ import entity.invoice.Invoice;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 import utils.Utils;
 import views.screen.BaseScreenHandler;
@@ -14,6 +17,8 @@ import views.screen.ViewsConfig;
 import views.screen.popup.PopupScreen;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Map;
 import java.util.logging.Logger;
 
@@ -42,13 +47,27 @@ public class PaymentScreenHandler extends BaseScreenHandler {
 	private TextField expirationDate;
 
 	@FXML
-	private TextField securityCode;
+	private VBox inputLayout;
+
+//	@FXML
+//	private TextField securityCode;
+
+	@FXML
+	private RadioButton methodCreditCard;
 //Stamp
 	public PaymentScreenHandler(Stage stage, String screenPath, Invoice invoice) throws IOException {
 		super(stage, screenPath);
 		try {
 			setupData(invoice);
 			setupFunctionality();
+			if(methodCreditCard.isSelected()){
+				TextField textField = new TextField();
+				textField.setFont(new Font(24));
+				Label label = new Label();
+				label.setFont(new Font(24));
+				label.setText("Card security code:");
+				inputLayout.getChildren().addAll(new ArrayList<>(Arrays.asList(label, textField)));
+			}
 		} catch (IOException ex) {
 			LOGGER.info(ex.getMessage());
 			PopupScreen.error("Error when loading resources.");
@@ -71,14 +90,24 @@ public class PaymentScreenHandler extends BaseScreenHandler {
 				System.out.println(exp.getStackTrace());
 			}
 		});
+
+		methodCreditCard.setOnAction(actionEvent -> {
+			if(methodCreditCard.isSelected()){
+				TextField textField = new TextField();
+				Label label = new Label();
+				label.setText("Card security code:");
+				inputLayout.getChildren().addAll(new ArrayList<>(Arrays.asList(label, textField)));
+			}
+		});
 	}
+
+   private
 
 	void confirmToPayOrder() throws IOException{
 		String contents = "pay order";
 		PaymentController ctrl = (PaymentController) getBController();
 		Map<String, String> response = ctrl.payOrder(invoice.getAmount(), contents, cardNumber.getText(), holderName.getText(),
-				expirationDate.getText(), securityCode.getText());
-
+				expirationDate.getText(), "1");
 		BaseScreenHandler resultScreen = new ResultScreenHandler(this.stage, ViewsConfig.RESULT_SCREEN_PATH, response);
 		resultScreen.setPreviousScreen(this);
 		resultScreen.setHomeScreenHandler(homeScreenHandler);
