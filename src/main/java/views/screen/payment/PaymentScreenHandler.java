4e5fc28e27ae0344f35437f0bba72d454345eff2
vietnb <vietlan2511@gmail.com>
2021-05-20 16:14:33 +0700

    update strategy pattern

--- src/main/java/views/screen/payment/PaymentScreenHandler.java
+++ src/main/java/views/screen/payment/PaymentScreenHandler.java
@@ -8,8 +8,8 @@ import javafx.scene.control.Label;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 import utils.Utils;
 import views.screen.BaseScreenHandler;
@@ -17,92 +17,101 @@ import views.screen.ViewsConfig;
 import views.screen.popup.PopupScreen;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Map;
 import java.util.logging.Logger;
 
 public class PaymentScreenHandler extends BaseScreenHandler {
 
-    private static final Logger LOGGER = Utils.getLogger(PaymentScreenHandler.class.getName());
+	private static final Logger LOGGER = Utils.getLogger(PaymentScreenHandler.class.getName());
 
-    @FXML
-    private Button btnConfirmPayment;
+	@FXML
+	private Button btnConfirmPayment;
 
-    @FXML
-    private ImageView loadingImage;
+	@FXML
+	private ImageView loadingImage;
 
-    private Invoice invoice;
+	private Invoice invoice;
 
-    @FXML
-    private Label pageTitle;
+	@FXML
+	private Label pageTitle;
 
-    @FXML
-    private TextField cardNumber;
+	@FXML
+	private TextField cardNumber;
 
-    @FXML
-    private TextField holderName;
+	@FXML
+	private TextField holderName;
 
-    @FXML
-    private TextField expirationDate;
+	@FXML
+	private TextField expirationDate;
 
-    @FXML
-    private VBox vboxItems;
+	@FXML
+	private VBox inputLayout;
 
-    private PaymentInputScreenHandler inputScreenHandler;
+//	@FXML
+//	private TextField securityCode;
 
-    @FXML
-    RadioButton methodCreditCart;
+	@FXML
+	private RadioButton methodCreditCard;
+//Stamp
+	public PaymentScreenHandler(Stage stage, String screenPath, Invoice invoice) throws IOException {
+		super(stage, screenPath);
+		try {
+			setupData(invoice);
+			setupFunctionality();
+			if(methodCreditCard.isSelected()){
+				TextField textField = new TextField();
+				textField.setFont(new Font(24));
+				Label label = new Label();
+				label.setFont(new Font(24));
+				label.setText("Card security code:");
+				inputLayout.getChildren().addAll(new ArrayList<>(Arrays.asList(label, textField)));
+			}
+		} catch (IOException ex) {
+			LOGGER.info(ex.getMessage());
+			PopupScreen.error("Error when loading resources.");
+		} catch (Exception ex) {
+			LOGGER.info(ex.getMessage());
+			PopupScreen.error(ex.getMessage());
+		}
+	}
 
-    //Stamp
-    public PaymentScreenHandler(Stage stage, String screenPath, Invoice invoice) throws IOException {
-        super(stage, screenPath);
-        try {
-            setupData(invoice);
-            setupFunctionality();
-        } catch (IOException ex) {
-            LOGGER.info(ex.getMessage());
-            PopupScreen.error("Error when loading resources.");
-        } catch (Exception ex) {
-            LOGGER.info(ex.getMessage());
-            PopupScreen.error(ex.getMessage());
-        }
-    }
+	protected void setupData(Object dto) throws Exception {
+		this.invoice = (Invoice) dto;
+	}
 
-    protected void setupData(Object dto) throws Exception {
-        this.invoice = (Invoice) dto;
-        setInputScreenHandler(new CreditCardInputScreenHandler(ViewsConfig.INPUT_PAYMENT));
-    }
+	protected void setupFunctionality() throws Exception {
+		btnConfirmPayment.setOnMouseClicked(e -> {
+			try {
+				confirmToPayOrder();
+				((PaymentController) getBController()).emptyCart();
+			} catch (Exception exp) {
+				System.out.println(exp.getStackTrace());
+			}
+		});
 
-    protected void setupFunctionality() throws Exception {
-        btnConfirmPayment.setOnMouseClicked(e -> {
-            try {
-                confirmToPayOrder();
-                ((PaymentController) getBController()).emptyCart();
-            } catch (Exception exp) {
-                System.out.println(exp.getStackTrace());
-            }
-        });
-        methodCreditCart.setOnMouseClicked(e -> {
-        });
-    }
+		methodCreditCard.setOnAction(actionEvent -> {
+			if(methodCreditCard.isSelected()){
+				TextField textField = new TextField();
+				Label label = new Label();
+				label.setText("Card security code:");
+				inputLayout.getChildren().addAll(new ArrayList<>(Arrays.asList(label, textField)));
+			}
+		});
+	}
 
-    private void setInputScreenHandler(PaymentInputScreenHandler inputScreenHandler) {
-        this.inputScreenHandler = inputScreenHandler;
-        updateView();
-    }
+   private
 
-    private void updateView() {
-        vboxItems.getChildren().clear();
-        vboxItems.getChildren().add(inputScreenHandler.getContent());
-    }
-
-    void confirmToPayOrder() throws IOException {
-        String contents = "pay order";
-        PaymentController ctrl = (PaymentController) getBController();
-        Map<String, String> response = ctrl.payOrder(invoice.getAmount(), contents, inputScreenHandler.getPaymentCard());
-        BaseScreenHandler resultScreen = new ResultScreenHandler(this.stage, ViewsConfig.RESULT_SCREEN_PATH, response);
-        resultScreen.setPreviousScreen(this);
-        resultScreen.setHomeScreenHandler(homeScreenHandler);
-        resultScreen.setScreenTitle("Result Screen");
-        resultScreen.show();
-    }
+	void confirmToPayOrder() throws IOException{
+		String contents = "pay order";
+		PaymentController ctrl = (PaymentController) getBController();
+		Map<String, String> response = ctrl.payOrder(invoice.getAmount(), contents, cardNumber.getText(), holderName.getText(),
+				expirationDate.getText(), "1");
+		BaseScreenHandler resultScreen = new ResultScreenHandler(this.stage, ViewsConfig.RESULT_SCREEN_PATH, response);
+		resultScreen.setPreviousScreen(this);
+		resultScreen.setHomeScreenHandler(homeScreenHandler);
+		resultScreen.setScreenTitle("Result Screen");
+		resultScreen.show();
+	}
 }
